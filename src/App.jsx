b9be927f42a7a6a587ce47993d9a09e0b2339b81
kon-amp <kon.ampatzidis@gmail.com>
2024-11-20 12:58:23 +0200

    feat: component composition by using children prop and reacting to events by passing functions as values to props

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import { CORE_CONCEPTS } from "./data.js";
 import Header from './components/Header/Header.jsx';
 import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButton.jsx";
 
 function App() {
+  function handleSelect() {
+    console.log('Hello World - selected!');
+  }
+
   return (
     <div>
       <Header />
@@ -19,6 +24,16 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={handleSelect}>Components</TabButton>
+            <TabButton onSelect={handleSelect}>JSX</TabButton>
+            <TabButton onSelect={handleSelect}>Props</TabButton>
+            <TabButton onSelect={handleSelect}>State</TabButton>
+          </menu>
+
         </section>
       </main>
     </div>
