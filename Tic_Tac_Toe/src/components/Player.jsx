6a6809e07429272c29c331d3973db23545b0c7b5
kon-amp <kon.ampatzidis@gmail.com>
2025-01-07 17:03:10 +0200

    chore: keep the players name as state so we can show them at the gameover message

--- Tic_Tac_Toe/src/components/Player.jsx
+++ Tic_Tac_Toe/src/components/Player.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 
-export default function Player({initialName, symbol, isActive}) {
+export default function Player({initialName, symbol, isActive, onChangeName}) {
   const [ playerName, setPlayerName ] = useState(initialName);
   const [ isEditing, setIsEditing ] = useState(false);
 
@@ -19,6 +19,10 @@ export default function Player({initialName, symbol, isActive}) {
      * works correctly, even if React batches or delays updates.
      */
     setIsEditing((editing) => !editing);
+
+    if (isEditing){
+      onChangeName(symbol, playerName);
+    }
   }
 
   function handleChange(eventObject) {
